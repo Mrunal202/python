758386b70031b3319cad2c6302d1017aa49ed641
Mrunal202 <89466689+Mrunal202@users.noreply.github.com>
2021-08-24 20:24:51 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=10.5
+print(type(a))
+c="mruna"
+print(type(c))
+print(c)
+print(str(a)+c)
+b=20
+c=a+b
+print("result is",c)
+print(c)
+
+a="mrunal vaidya"
+a.upper()
+print(a.title())
+print(a.lower())
+print(a.isdigit())
