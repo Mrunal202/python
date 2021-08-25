9e90574c83d5f19d0ee25476f168ec226bc55468
Mrunal202 <89466689+Mrunal202@users.noreply.github.com>
2021-08-25 10:13:47 -0700

    Add files via upload

--- list.py
+++ list.py
@@ -13,12 +13,7 @@ type(list1)
 print("type of list1 is:",type(list1))
 
 
-list2=[5,6,9,'xyz','python']
 
-
-print("list2 is:",list2)
-print("type of list2 is:",type(list2))
-print("length of list2:",len(list2))
 
 
 
@@ -39,20 +34,73 @@ print("Printing list [::3]",list1[::3]) #   [1, 'A']
 
 
 
+list2=[5,6,9,'xyz','python',10.5,59]
+
+
+print("list2 is:",list2)
+print("type of list2 is:",type(list2))
+print("length of list2:",len(list2))
+
+
+print(list2[-1:-5:-1])  #[59, 10.5, 'python', 'xyz']
+list2[-2:3:-1]   #   [10.5, 'python']
+
 #reverse
 print("reverse of list is",list1[::-1])
 list1.reverse()
 
-list1.append(22)
-print(list1)
-
-list1.remove(22)
-print(list1)
-
-list1.pop(0)
-print(list1)
 
 
 
-list1[2]=55
-print(list1)
+
+list2.append(22)
+print(list2)
+
+list2[7]=89
+print(list2)
+
+list2[2:4]=20,"mrunal"
+print(list2)
+
+list2.insert(4, """cpp""")
+print(list2)
+list2.remove("cpp")
+print(list2)
+
+list2.pop(6)
+print(list2)
+
+
+
+list2[2]=55
+print(list2)
+
+'''' to check given element in list'''
+
+if 58 in list2:
+    print("yes")
+else:
+    print("no")
+    
+#extend
+list3=[10]
+list3.extend(list2)
+print(list3)
+
+
+
+#print using for
+for x in list2:
+    print(x)
+
+#print using while
+
+i=0
+while i<len(list3):
+    print(list3[i])
+    i=i+1
+    
+    
+    
+list2.count(89)
+list2.__contains__(89)
