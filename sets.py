b013f252886e54938db2937436545c0abbe791fc
Mrunal202 <89466689+Mrunal202@users.noreply.github.com>
2021-08-31 06:29:33 -0700

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 31 05:59:40 2021
+
+@author: HP
+"""
+
+p={1,2,3}
+p.add(50)
+p.add(55)
+print(p)
+d={"a":10,"b":20,"c":30}
+s=set(d)
+
+s.add(56)
+s.add("n")
+print(s)
+
+s.remove("b")
+
+s.pop()
+
+s.clear()
+print(s)
+
+
+
+
+s1=(13,24,56,78)
+s2=(45,67,89,99)
