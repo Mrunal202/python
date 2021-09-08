3f6cc0c37166e9b76869bf2763ff6e7cc756ff22
Mrunal202 <89466689+Mrunal202@users.noreply.github.com>
2021-09-08 07:02:21 -0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -135,3 +135,136 @@ def sumlist(l1):
 l1=[1,2,3,4]
 print("Sum of list elements is",sumlist(l1))
 
+
+#swapping numbers fron list
+def swaplist(list1):
+    temp=list1[0]
+    list1[0]=list1[-1]
+    list1[-1]=temp
+    return list1
+
+list1=[10,20,30,45]
+print("list after swapping is:",swaplist(list1))
+
+
+def swaplist(list1):
+    for i in range(0,len(list1)-1):
+         # temp=list1[i]
+       # list1[i]=list1[i-1]
+        #list1[i-1]=temp
+        list1[i],list1[-1-i]=list1[-1-i],list1[i]
+    
+    return list1
+
+list1=[10,20,30,45,60]
+print("list after swapping is:",swaplist(list1))
+
+
+
+###############SWAPPING WITH POSITIONS
+
+def swappos(list1,pos1,pos2):
+    list1[pos1],list1[pos2]=list1[pos2],list1[pos1]
+    return list1
+
+list1=[10,20,30,40]
+pos1=int(input("Enter position1"))
+pos2=int(input("enter position2"))
+print("after swapping elements list1 is:",swappos(list1,pos1,pos2))
+
+
+####reversing the words
+sen="mrunal vaidya"
+words=sen.split(" ")
+words=list(reversed(words))
+print("".join(words))
+print(words)
+
+#random number from list
+import random
+list1=[1,2,3,5,6]
+print(random.choice(list1))
+
+
+#enumerator
+list1=["apple","banana","orange"]
+enum1=enumerate(list1)
+print(list(enum1))
+
+for j in enumerate(list1):
+    print(list(j))
+type(enumerate(list1))
+
+for count,item in enumerate(list1,100):
+    print(count,item)
+    
+    
+    
+    
+    #addition of two numbers
+def add1(a,b):
+    return a+b
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("addition is",add1(a,b))
+
+ ##################LAMBDA FUNCTION#############
+ ###addition
+sum1 =lambda x,y: x+y
+x=int(input("enter the value1:"))
+y=int(input("enter the value2:"))
+print(sum1(x,y))
+
+##area
+area1=lambda r: 3.14*r*r
+r=int(input("enter radius:"))
+print("area of circle is",area1(r)) 
+
+##square
+import math
+def square(n):
+    return math.pow(n, 2)
+
+print("square is:",square(5))
+
+square1=lambda n: n**2
+print("square is:",square1(10))
+type(square1)
+
+
+def add3(x,y,z):
+    return x+y+z
+
+print("addition is",add3(10,40,50))
+
+add3=lambda x,y,z: x+y+z
+print("addition is",add3(3,3,1))
+
+
+
+######MAP FUNCTION####
+sqr= map(lambda x:x*x,list1)
+list1=[10,20,30]
+type(sqr)
+print("square is",list(sqr))
+
+sqr1=map(lambda x:x*x,d1.values())
+d1={"a":1,"b":2}
+print("square is",list(sqr1))
+
+
+
+L=[lambda x:x*x,lambda x:x*x*x, lambda x:x*x*x*x*x]
+for f in L:
+    p=f(4)
+    print(p)
+    
+    
+    
+    
+list1=[10,20,30,40]
+list2=[10,20,30,40]
+
+p=map(lambda x,y:x+y ,list1,list2)
+print(list(p))
